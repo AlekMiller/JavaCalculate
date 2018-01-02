4d6a958ea6760ed16a1b6930957447e6c84ac0c9
unknown <melnyk.oleksandr1996@gmail.com>
2018-01-02 13:53:17 +0100

    add

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,8 @@
 public class Calculate {
 	public static void main (String [] arg){
-System.out.print ("Hello world");
+	int first = Integer.valueOf(arg[0]);
+	int second = Integer.valueOf(arg[1]);
+	int sum = first + second;
+	System.out.print ("Sum "+sum);
 	}
 }
